8631cd9a68a236351fa215075276a1f7a23ecbb8
Irina programmed <krasnova.i@gmail.com>
2022-12-10 08:12:59 +0100

    task41

---
+++ T41/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+Console.Write($"Введите число количество чисел для анализа: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[m];
+
+void InputNumbers(int m){
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введите число {i+1}: ");
+    massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int Comparison(int[] massiveNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < massiveNumbers.Length; i++)
+  {
+    if(massiveNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(m);
+
+Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
